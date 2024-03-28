b9665e34dc4410c78002368e04fe4c88396b47ac
ekene samuel <chikwenduekene@gmail.com>
2024-03-28 14:03:55 +0000

    binary tree complete

---
+++ 114-bst_remove.c
@@ -0,0 +1,19 @@
+#include "binary_trees.h"
+
+bst_t *inorder_successor(bst_t *root);
+bst_t *bst_delete(bst_t *root, bst_t *node);
+bst_t *bst_remove_recursive(bst_t *root, bst_t *node, int value);
+bst_t *bst_remove(bst_t *root, int value);
+
+/**
+ * inorder_successor - Returns the minimum value of a binary search tree.
+ * @root: A pointer to the root node of the BST to search.
+ *
+ * Return: The minimum value in @tree.
+ */
+bst_t *inorder_successor(bst_t *root)
+{
+	while (root->left != NULL)
+		root = root->left;
+	return (root);
+}
